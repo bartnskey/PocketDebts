fd3e8e9931ab7d2066e68d8a6f58fa5725e67ab3
BartNskey <lopuszanski.bartosz@gmail.com>
2023-05-08 23:06:27 +0200

    1. Abstract class 'Loan' created, define methods with algorytms

---
+++ Loan.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanKnot
+{
+    abstract class Loan
+    {
+        public int loanAmount { get; set; }
+
+        public int instalmentQuantity { get; set; }
+
+        public string instalmentType { get; set; }
+
+        public double instalmentAmount { get; set; }
+
+        public double loanFee { get; set; }
+
+        public double interestRate { get; set; }
+
+        public double interestRateOverpayment { get; set; }
+
+        public double interestAmount { get; set; }
+
+        public int overpayment { get; set; }
+
+        public double totalLoanAmount { get; set; }
+
+        public double totalInterestAmount { get; set; }
+
+        public double capitalAmount { get; set; }
+
+        public int monthsPassed { get; set; }
+
+
+        public void RealInterestRate()
+        {
+            double realInterestRate = totalInterestAmount / loanAmount * 100;
+        }
+
+        public void InterestAmount()
+        {
+            double interestAmount = loanAmount * interestRate / 12;
+        }
+
+        public void InterestAmountOverpayment()
+        {
+            double interestAmountOverpayment = ((loanAmount - capitalAmount - overpayment) * interestRate / 100) / 12;
+        }
+
+        public void CapitalAmount()
+        {
+            double capitalAmount = instalmentAmount - interestAmount;
+        }
+
+        public void LoanFeeValue()
+        {
+            double loanFeeValue = loanAmount * loanFee;
+        }
+
+        public void LoanFeeValueOverpayment()
+        {
+            double loadnFeeValueOverpayment = overpayment * interestRateOverpayment;
+        }
+
+        public void TotalLoanAmount()
+        {
+            double totalLoanAmount = instalmentAmount * instalmentQuantity;
+        }
+
+        public void TotalInterestAmount()
+        {
+            double totalInterestAmount = totalLoanAmount - loanAmount;
+        }
+
+        public void LessInstalmentAmountOverpayment()
+        {
+            var lessInstalmentAmountOverpayment = (loanAmount * interestRate / 100 / 12 * (1 + interestRate / 100 / 12) ^ monthsPassed) / ((1 + interestRate / 100 / 12) ^ monthsPassed - 1);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LoanKnot
+{
+    class Program : Loan
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter load Amount");
+            int loanAmount = int.TryParse(Console.ReadLine());
+        }
+    }
+}
